3f7770fb6005acf2b0b4ced49bca6f9de37afd3a
Bryan <bkapicka@gmail.com>
2013-07-23 11:44:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedResources::Application.config.secret_token = 'e526f4fc99f512c56f35a68e109084cf836f5d9689c4c7e255de5afa1a5d83d9bffd76935048c1686369ece03a7ae0f041e807a78ef23935b88aaefd44f5b6bc'
