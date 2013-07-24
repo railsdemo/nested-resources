764dac9598db27a7a70676b68cfd6a68afa5f938
Bryan <bkapicka@gmail.com>
2013-07-24 09:49:00 -0400

    show user to include user notepads and notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.json
+  # GET
   def index
     @users = User.all
 
@@ -10,19 +9,18 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
+  # GET
   def show
     @user = User.find(params[:id])
-
+    @notepads = @user.notepads
+    @notes = @user.notes
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
+  # GET
   def new
     @user = User.new
 
@@ -32,13 +30,12 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1/edit
+  # GET
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
+  # POST
   def create
     @user = User.new(params[:user])
 
@@ -53,8 +50,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
+  # PUT
   def update
     @user = User.find(params[:id])
 
@@ -69,8 +65,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+  # DELETE
   def destroy
     @user = User.find(params[:id])
     @user.destroy
